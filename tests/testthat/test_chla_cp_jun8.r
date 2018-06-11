e9fbae91bfa35cfa64128e14966fa1084fbbeede
bc <brian.cohn@usc.edu>
2018-06-11 14:33:20 -0700

    check works

--- tests/testthat/test_chla_cp_jun8.r
+++ tests/testthat/test_chla_cp_jun8.r
@@ -3,5 +3,4 @@ test_that("chla_CP patient data functional",{
 	filepath <- "/Users/briancohn/Resilio\ Sync/CHLA_data/trial_4.txt"
 	df <- read_json(filepath) %>% convert_json_to_df
 	write.csv(df, "../../output/trial_4.csv", row.names=FALSE)
-	tall_df <- melt(df, id.vars="timestamp")
 })
